626127caa1a00707f7460634f27b9b73e28d41b8
Arnesh Barik <meet.arnesh@gmail.com>
2026-04-24 05:41:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3685922975-dd505",
+  "appId": "1:1095363982818:web:0fff23c15cf92e6dd8b5bb",
+  "apiKey": "AIzaSyDGda54X7F_fbfjhmZUnKJSD0pOWSXMfYs",
+  "authDomain": "studio-3685922975-dd505.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1095363982818"
+};
